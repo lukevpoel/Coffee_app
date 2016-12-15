a55215fa41d7ee4416a49bfcbce8a17987c90965
Luke Vander Poel <airscout99@gmail.com>
2016-12-14 22:02:35 -0800

    installing angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coffeeAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the coffeeAppApp
+ */
+angular.module('coffeeAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
